ce0f2ce829248c84c20ab26f6c495c6c16b25166
Andy Ngono <ngonoarnaudandy@gmail.com>
2024-10-25 09:14:08 +0100

    modfied commit in the new branch

---
+++ src/main/java/stjean/OperationMathematique.java
@@ -0,0 +1,14 @@
+package stjean;
+
+public class OperationMathematique {
+
+    public boolean  estPositif(int number) {
+        if(number >= 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
