1fe478dddba6f25adf00080a5e41b99ca3c6b408
Andy Ngono <ngonoarnaudandy@gmail.com>
2024-10-25 10:19:19 +0100

    sort method commit

--- src/main/java/stjean/OperationMathematique.java
+++ src/main/java/stjean/OperationMathematique.java
@@ -1,6 +1,7 @@
 package stjean;
 
-import
+
+
 public class OperationMathematique {
 
     public boolean  estPositif(int number) {
@@ -20,5 +21,20 @@ public class OperationMathematique {
         else {
             return number * (factorial(number-1));
         }
+    }
+
+    public static int[] sort(int[] list) {
+        // Implementing bubble sort in descending order
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = 0; j < list.length - 1 - i; j++) {
+                if (list[j] < list[j + 1]) {
+                    // Swap elements if they are in the wrong order
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+        return list;
     }
 }
