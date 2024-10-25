bbd5c841f0e852b72d4907be49a77c6169b695ca
Andy Ngono <ngonoarnaudandy@gmail.com>
2024-10-25 09:30:19 +0100

    second commit in the new branch operation

--- src/main/java/stjean/OperationMathematique.java
+++ src/main/java/stjean/OperationMathematique.java
@@ -1,5 +1,6 @@
 package stjean;
 
+import
 public class OperationMathematique {
 
     public boolean  estPositif(int number) {
@@ -10,5 +11,14 @@ public class OperationMathematique {
             return false;
         }
 
+    }
+    public int factorial (int number){
+
+        if (number ==  0|| number ==  1){
+            return 1;
+        }
+        else {
+            return number * (factorial(number-1));
+        }
     }
 }
